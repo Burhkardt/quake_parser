e6e720645f36cb83476b5d4a749e2bdea56d6bd7
Juliandre César <juliandrecesar@gmail.com>
2026-02-27 09:35:38 -0300

    Adicionado leitor do log
    Responsável por agregar todas os objetos de partidas e popular players e kills nos mesmos.

--- parser.py
+++ parser.py
@@ -4,11 +4,13 @@ class Game:
         self.total_kills = 0
         self.players = set()
         self.kills = {}
+
     def add_player(self, nome):
         if nome != "<world>":
             self.players.add(nome)
             if nome not in self.kills:
                 self.kills[nome] = 0
+
     def count_kills(self, killer, victim):
         self.total_kills += 1
         self.add_player(victim)
@@ -17,9 +19,31 @@ class Game:
         else:
             self.add_player(killer)
             self.kills[killer] += 1
+
     def dict_converter(self):
         return {
             "total_kills": self.total_kills,
             "players": list(self.players),
             "kills": self.kills
             }
+    
+
+class Parser:
+    def __init__(self, diretorio):
+        self.diretorio = diretorio
+        self.partidas = {}
+
+    def leitor(self):
+        game_number = 0
+        with open(self.diretorio, 'r') as log:
+            for l in log:
+                linha_tratada = l.strip()
+                if "initGame" in linha_tratada:
+                    game_number += 1
+                    game = Game(game_number)
+                    self.partidas[game.partida] = game
+                elif "Kill" in linha_tratada:
+                    kill_text = l.split(":")[3]
+                    killer = kill_text.split("killed")[0].strip()
+                    victim = kill_text.split("killed")[1].split("by")[0].strip()
+                    game.count_kills(killer, victim)
